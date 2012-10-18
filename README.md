a7b7b13287fe0389ee3f0d5300686ec6fb6dd65a
Lauri Rooden <lauri@rooden.ee>
2012-10-18 15:18:10 +0300

    Should be working. Testing needed

--- README.md
+++ README.md
@@ -1,51 +1,51 @@
-Set Up A Git Server
--------------------
+Install
+-------
 
-```
-$ sudo adduser git
-$ su git
-$ cd /home/git/
-$ mkdir .ssh
-$ chmod 0700 ~/.ssh
-$ touch ~/.ssh/authorized_keys
-$ chmod 0600 ~/.ssh/authorized_keys
-$ wget -O gitserv.sh https://raw.github.com/lauriro/gitserv.sh/devel/gitserv.sh
-$ chmod +x gitserv.sh
-# Use full path here!
-$ /home/git/gitserv.sh user add richard 'ssh-rsa AAAAB3NzaC1yc2E...50i8Q== richard@example.com'
-$ /home/git/gitserv.sh user group richard 'all,admin'
-$ exit
-```
+- Create a user `git`
+- Prepare `git` ssh config
 
-### Manage users
+        $ cd /home/git/
+        $ mkdir .ssh
+        $ chmod 0700 ~/.ssh
+        $ touch ~/.ssh/authorized_keys
+        $ chmod 0600 ~/.ssh/authorized_keys
 
-```
-# List of users
-$ ssh git@repo.example.com user
-# Show richard's permissions
-$ ssh git@repo.example.com user show richard
-$ ssh git@repo.example.com user add richard 'sh-rsa AAAAB3N...50i8Q==' user@example.com
-$ ssh git@repo.example.com user group richard all,admin
-$ ssh git@repo.example.com user key richard 'sh-rsa AAAAB3N...50i8Q==' user@example.com
-$ ssh git@repo.example.com user del richard
-```
+- Get sshd wraper
 
-### Manage repos
+        $ wget -O gitserv.sh https://raw.github.com/lauriro/gitserv.sh/devel/gitserv.sh
+        $ chmod +x gitserv.sh
 
-```
-$ ssh git@repo.example.com repo add test.git
-$ ssh git@repo.example.com repo config test.git access.read all
-$ ssh git@repo.example.com repo config test.git access.write admin,richard
-$ ssh git@repo.example.com repo config test.git access.write.devel all
-$ ssh git@repo.example.com repo config test.git access.tag richard
-$ ssh git@repo.example.com repo config test.git branch.master.mergeoptions "--ff-only"
-$ ssh git@repo.example.com repo config test.git branch.master.denyDeletes true
-$ ssh git@repo.example.com repo config test.git branch.devel.mergeoptions "--no-ff"
-$ ssh git@repo.example.com repo config test.git tags.denyOverwrite true
-$ ssh git@repo.example.com repo config test.git --unset tags.denyOverwrite
-$ ssh git@repo.example.com repo del test.git
+- Make initial config
 
-```
+        $ ./gitserv.sh user richard add 'ssh-rsa AAAAB3NzaC1yc2E...50i8Q== richard@example.com'
+        $ ./gitserv.sh user richard group 'all,admin'
+
+
+Usage
+-----
+
+    $ ssh git@localhost help
+    Example usage:
+    
+      $ ssh git@host repo
+      $ ssh git@host repo test.git init
+      $ ssh git@host repo test.git config access.read all
+      $ ssh git@host repo test.git config access.write admin,richard
+      $ ssh git@host repo test.git config access.write.devel all
+      $ ssh git@host repo test.git config access.tag richard
+      $ ssh git@host repo test.git config branch.master.denyDeletes true
+      $ ssh git@host repo test.git config branch.master.mergeoptions "--ff-only"
+      $ ssh git@host repo test.git config branch.devel.mergeoptions "--no-ff"
+      $ ssh git@host repo test.git config tags.denyOverwrite true
+      $ ssh git@host repo test.git describe "My cool repo"
+      $ ssh git@host repo test.git fork new_repo.git
+      $ ssh git@host repo test.git drop
+      $ ssh git@host user
+      $ ssh git@host user richard
+      $ ssh git@host user richard add 'sh-rsa AAAAB3N...50i8Q==' user@example.com
+      $ ssh git@host user richard key 'sh-rsa AAAAB3N...50i8Q==' user@example.com
+      $ ssh git@host user richard group all,admin
+      $ ssh git@host user richard del
 
 
 ### Licence
--- gitserv.sh
+++ gitserv.sh
@@ -26,23 +26,38 @@ log() {
 
 deny() {
 	log "ERROR: $1: "
-	printf '\n***\n* ERROR: %s.\n***\n\n' "$1" >&2
+	printf "error: %s\n" "$1" >&2
 	exit 1
 }
+
 
 # deny Ctrl-C and unwanted chars
 trap "deny 'BYE';kill -9 $$" 1 2 3 6 15
 expr "$SSH_ORIGINAL_COMMAND$*" : '[-+ [:alnum:],'\''./_@=]*$' >/dev/null || deny "DON'T BE NAUGHTY"
-expr "$SSH_ORIGINAL_COMMAND$*" : '.*\.\.' >/dev/null && deny "DON'T BE EVIL"
-
 
 is_admin() {
 	[ -z "$SSH_CLIENT" ] || grep -q " USER=$USER GROUP=[^ ]*\badmin\b" $KEYS || deny 'Admin access denied'
 }
 
+is_secure() {
+	expr "$1" : '.*\.\.' >/dev/null && deny "DON'T BE EVIL"
+	expr "$1" : '[^[:alnum:]]' >/dev/null && deny "DON'T BE CRUEL"
+}
+
 conf() {
 	[ -n "$GIT_NAMESPACE" ] && FILE=$GIT_NAMESPACE || FILE="$R/config"
 	git config --file "$FILE" $@
+}
+
+list_of_repos() {
+	{
+		grep -Ilr --include=*config '^\s*bare = true' * 2>/dev/null | sed -e 's,/config$,,'
+		grep -Ir --include=*.git '^\s*master = .*' * 2>/dev/null | sed 's/:.*= / -> /'
+	} | sort
+}
+
+list_of_users() {
+	sed -E -e 's/^.* USER=([^ ]*) GROUP=([^ ]*) .*$/\1 \2/' $KEYS 
 }
 
 acc_re() {
@@ -54,22 +69,15 @@ acc() {
 	grep -E -q "$(acc_re $USER)" || deny "${2-"Repository not found"}"
 }
 
-list_of_repos() {
-	{
-		grep -Ilr --include=*config '^\s*bare = true' * 2>/dev/null | sed -e 's,/config$,,'
-		grep -I '^\s*master = .*' * 2>/dev/null | sed 's/:.*= / -> /'
-	} | sort
-}
-
-
 
 [ $# -eq 0 ] && set -- $SSH_ORIGINAL_COMMAND
 
 # unquoted repo name
 R=${2%\'};R=${R#*\'}
+is_secure "$R"
 
 # When repo is a file then it is a fork
-[ -f "$R" ] && GIT_NAMESPACE=$R && R=$(conf fork.master)
+[ -f "$R" ] && GIT_NAMESPACE=$R && R=$(conf fork.master) && is_secure "$R"
 
 #- Example usage:
 #- 
@@ -122,37 +130,53 @@ case $1 in
 #-   $ ssh git@host repo test.git config branch.master.mergeoptions "--ff-only"
 #-   $ ssh git@host repo test.git config branch.devel.mergeoptions "--no-ff"
 #-   $ ssh git@host repo test.git config tags.denyOverwrite true
+#-   $ ssh git@host repo test.git describe "My cool repo"
+#-   $ ssh git@host repo test.git fork new_repo.git
 #-   $ ssh git@host repo test.git drop
 
 		test -e "$R" -o "$3" = "init" -o -z "$3" || deny "Repository $R not found"
 		
 		case "$3" in
 			init)
-				#[ -e "$R" ] && deny "Repository exists"
+				[ -e "$R" ] && deny "Repository exists"
 				mkdir -p "$R" && \
 				cd "$R" >/dev/null && \
 				git init --bare -q && \
 				printf '#!/bin/sh\n%s update-hook $@\n' "$SELF" > hooks/update && \
 				chmod +x hooks/update
 			;;
-			drop)
-				# Backup repo
-				tar -czf "$2.$(date -u +'%Y%m%d%H%M%S').tar.gz" $2
-
-				rm -rf $2
-			;;
 			config)
 				conf ${4-'-l'} $5 >&2
 			;;
+			describe)
+				# TODO:2012-10-18:lauriro: Add namespaces support for description
+				[ "$R" = "$2" ] && {
+					shift 3
+					echo "$*" > $R/description
+				}
+			;;
+			drop)
+				is_secure "$2"
+				# Backup repo
+				tar -czf "$2.$(date -u +'%Y%m%d%H%M%S').tar.gz" $2
+
+				# TODO:2012-10-18:lauriro: Remove namespaced data from repo
+				rm -rf $2
+			;;
 			fork)
+				is_secure "$4"
+				[ -e "$4" ] && deny "Repository exists"
 				GIT_NAMESPACE="$4"
+				[ "${4%/*}" = "$4" ] || mkdir -p ${4%/*}
 				conf fork.master "$R"
 			;;
 			*)
-				[ -n "$2" -a -e $2 ] && {
+				[ -n "$2" -a -e "$2" ] && {
+					is_secure "$2"
+					[ "$R" = "$2" ] && cat $R/description
 					printf "\nDisk usage: %s\n\nRepo '%s' permissions:\n" "$(du -hs $2 | cut -f1)" "$2"
 					conf --get-regexp '^access\.' | sed -e 's,^access\.,,' -e 's/,/|/g' | while read name RE;do
-						printf "$name [$RE] - %s\n" "$(sed -E -e 's/^.* USER=([^ ]*) GROUP=([^ ]*) .*$/\1 \2/' $KEYS | grep -E "\\b($RE)\\b" | cut -d" " -f1 | sort | tr "\n" " ")"
+						printf "$name [$RE] - %s\n" "$(list_of_users | grep -E "\\b($RE)\\b" | cut -d" " -f1 | sort | tr "\n" " ")"
 					done
 				} >&2
 				printf "\nLIST OF REPOSITORIES:\n%s\n" "$(list_of_repos)" >&2
